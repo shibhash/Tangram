825446286d5730cbf273373673a585a5152f9e86
shibhash <sahooshib1@gmail.com>
2016-04-24 14:00:31 +0530

    Added files via upload

---
+++ js/index.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+            $(".selectable").draggable({
+                addClasses: false,
+                snap: false,
+                stack: ".destination",
+				scroll: true
+               
+            });
+
+            $(".destination").draggable({
+                snapMode: "inner"
+            });
+
+            $(".destination").draggable("disable");
+
+            $(".destination").droppable({
+                drop: function (event, ui) {
+                    var selectedShape = ui.draggable.attr("id");
+                    var dropZone = $(this).attr("id");
+                    dropZone = dropZone.replace("inside", "");
+                    if(selectedShape == dropZone)
+                    {
+                        $("#" + selectedShape).draggable("disable");
+                        checkShapeStatus();
+                    }
+                    else {
+                        alert("Wrong choice!");
+                    }
+                }
+            });
+        });
+        function checkShapeStatus() {
+            var counter = 0;
+            $(".selectable").each(function () {
+                var $thisId = $(this);
+                var booleanValue = $thisId.draggable('option', 'disabled');
+                if (booleanValue)
+                {
+                    counter = counter + 1;
+                }
+                else {
+                }
+                if(counter == 7)
+                {
+                    $("#centerText").text('You win!');
+                    $("#centerText").fadeIn(1000).fadeOut(1000).fadeIn(1000).fadeOut(1000).fadeIn(1000);
+                }
+            })
+        }
